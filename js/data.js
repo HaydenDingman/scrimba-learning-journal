7e5f891c6b32aeb65c19eceacc1023a820cfa667
Hayden Dingman <haydendingman@gmail.com>
2025-05-26 23:16:35 -0700

    Fix image paths

--- js/data.js
+++ js/data.js
@@ -3,42 +3,42 @@ export const blogposts = [
         title: "Blog one",
         date: "2022-07-23",
         subhed: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        image: "images/desk.png",
+        image: "./images/desk.png",
         alt: "A laptop on a desk"
     },
     {
         title: "Blog two",
         date: "2022-07-23",
         subhed: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        image: "images/lightbulb.png",
+        image: "./images/lightbulb.png",
         alt: "A person holding a lightbulb"
     },
     {
         title: "Blog three",
         date: "2022-07-23",
         subhed: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        image: "images/abstract.png",
+        image: "./images/abstract.png",
         alt: "An abstract splash of color."
     },
     {
         title: "Blog four",
         date: "2022-07-23",
         subhed: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        image: "images/beach.png",
+        image: "./images/beach.png",
         alt: "A woman on a beach."
     },
     {
         title: "Blog five",
         date: "2022-07-23",
         subhed: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        image: "images/science.png",
+        image: "./images/science.png",
         alt: "A laptop with some code displayed."
     },
     {
         title: "Blog six",
         date: "2022-07-23",
         subhed: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        image: "images/outside.png",
+        image: "./images/outside.png",
         alt: "A laptop sitting on a picnic table outside."
     },
 ]
