b1436d7ead360a6f1baf369c26109c2a5bc1147f
Hayden Dingman <haydendingman@gmail.com>
2025-05-26 23:06:39 -0700

    Add project files from Scrimba

---
+++ js/index.js
@@ -0,0 +1,41 @@
+import { blogposts } from "./data.js"
+
+const navBar = document.getElementById("navigation-bar");
+
+document.getElementById("navigation-menu").addEventListener("click", openNavMenu);
+
+document.addEventListener("click", () => {
+    if (!navBar.classList.contains("hide-nav-bar")) {
+        navBar.classList.toggle("hide-nav-bar");
+    }
+})
+
+function openNavMenu(e) {
+    e.stopPropagation();
+    navBar.classList.toggle("hide-nav-bar");
+}
+
+function getBlogRollHtml() {
+    return blogposts.map((post) => {
+        const publishDate = new Date(post.date);
+        let options = {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        }
+        return `<div class="article-card">
+                    <a href="#">
+                        <img class="card-image" alt="${post.alt}" src="${post.image}" />
+                        <time datetime="${post.date}">${new Intl.DateTimeFormat("en-US", options).format(publishDate)}</time>
+                        <h2>${post.title}</h2>
+                        <p>${post.subhed}</p>
+                    </a>
+                </div>`
+    }).join("");
+}
+
+function render() {
+    document.getElementById("blog-roll").innerHTML = getBlogRollHtml();
+}
+
+render();
---
+++ js/recentPosts.js
@@ -0,0 +1,42 @@
+import { blogposts } from "./data.js"
+
+const navBar = document.getElementById("navigation-bar");
+
+document.getElementById("navigation-menu").addEventListener("click", openNavMenu);
+
+document.addEventListener("click", () => {
+    if (!navBar.classList.contains("hide-nav-bar")) {
+        navBar.classList.toggle("hide-nav-bar");
+    }
+})
+
+function openNavMenu(e) {
+    e.stopPropagation();
+    navBar.classList.toggle("hide-nav-bar");
+}
+
+
+function getBlogRollHtml() {
+    return blogposts.map((post) => {
+        const publishDate = new Date(post.date);
+        let options = {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        }
+        return `<div class="article-card">
+                    <a href="#">
+                        <img class="card-image" alt="${post.alt}" src="${post.image}" />
+                        <time datetime="${post.date}">${new Intl.DateTimeFormat("en-US", options).format(publishDate)}</time>
+                        <h2>${post.title}</h2>
+                        <p>${post.subhed}</p>
+                    </a>
+                </div>`
+    }).join("");
+}
+
+document.getElementById('view-more').addEventListener('click', viewMoreClick);
+
+function viewMoreClick() {
+    document.getElementById("blog-roll").innerHTML = `<h4 class="recent-posts-title blog-subhed">Recent Posts</h4>${getBlogRollHtml()}`;
+}
